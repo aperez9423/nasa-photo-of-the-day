0298a09b556b4cd9096bb26f75aadf94a41441b6
Angelica Perez <aperez9423@gmail.com>
2020-06-24 22:07:23 -0400

    added styles and figured out how to make the API request work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from "react";
 import "./App.css";
 import MediaCard from "./Components/MediaCard.js";
+import styled from 'styled-components'
 
+const Background = styled.div`
+  background: lavender;
+`
+const Title = styled.h1`
+  font-size: 2rem;
+  background-color: slateblue;
+  border-bottom: solid .2rem black;
+  border-top: solid .2rem black;
+  text-align: center;
+  padding: .5rem;
+  font-family: oxygen;
+`
 function App() {
   return (
-    <div className="App">
-      <MediaCard/>
-    </div>
+    <Background>
+        <Title>NASA Photo of the Day</Title>
+        <div className="App">
+        <MediaCard/>
+        </div>
+    </Background>
   );
 }
 
--- src/Components/MediaCardInfo.js
+++ src/Components/MediaCardInfo.js
@@ -1,12 +1,36 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
-import MediaCard from "./MediaCard";
+import styled from 'styled-components'
+
+const Image = styled.img`
+    height: 40vh;
+    width: 75vw;
+    border: solid .3rem slateblue;
+`
+const Heading = styled.h2`
+    font-size: 1.6rem;
+    font-family: oxygen;
+`
+const Info = styled.h4`
+    font-size: 1.2rem;
+    font-family: oxygen;
+`
+const Paragraph = styled.p`
+    font-size: 1.1rem;
+    font-family: oxygen;
+    margin: 2rem;
+`
+const Span = styled.span`
+    font-size: 1.1rem;
+    font-family: oxygen;
+    font-weight: Bolder;
+`
 
 export default function MovieCard () {
     const [photo, setPhoto] = useState([]);
     useEffect(() => {
         axios
-            .get(`https://api.nasa.gov/planetary/apod?api_key=h2IFiYwRI55fbDe4Jd7nxFHQGpjlYEwA9MLIJ9ai`)
+            .get(`https://api.nasa.gov/planetary/apod?api_key=h2IFiYwRI55fbDe4Jd7nxFHQGpjlYEwA9MLIJ9ai&date=2020-06-24`)
             .then(res => {
                 console.log ("Res from useEffect of MediaCard", res);
                 setPhoto(res.data);
@@ -17,10 +41,12 @@ export default function MovieCard () {
     }, []);
 
     return (
-        <div className = "card-container">
-            <div className = "image-container">
-                <MediaCard image={photo}/>
-            </div>
+        <div>
+            <Heading>{photo.title}</Heading>
+            <Info>Date: {photo.date}</Info>
+            <Image src= {photo.hdurl} alt= "space"/>
+            <Info>Image Credit and Copyright: {photo.copyright}</Info>
+            <Paragraph><Span>Explanation: </Span>{photo.explanation}</Paragraph>
         </div>
     );
 };
